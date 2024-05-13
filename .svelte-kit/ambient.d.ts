ec592f8dec74673cc1f2e7f24663d4a4cea9c489
Matthew Dalquist <102756004+mdalquist@users.noreply.github.com>
2024-05-12 19:22:00 -0700

    points plotted

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -57,6 +57,7 @@ declare module '$env/static/private' {
 	export const MSYSTEM_CHOST: string;
 	export const MSYSTEM_PREFIX: string;
 	export const NODE: string;
+	export const NODE_ENV: string;
 	export const npm_command: string;
 	export const npm_config_cache: string;
 	export const npm_config_globalconfig: string;
@@ -73,15 +74,8 @@ declare module '$env/static/private' {
 	export const npm_lifecycle_event: string;
 	export const npm_lifecycle_script: string;
 	export const npm_node_execpath: string;
-	export const npm_package_dev: string;
-	export const npm_package_dev_optional: string;
-	export const npm_package_engines_node: string;
-	export const npm_package_integrity: string;
 	export const npm_package_json: string;
 	export const npm_package_name: string;
-	export const npm_package_optional: string;
-	export const npm_package_peer: string;
-	export const npm_package_resolved: string;
 	export const npm_package_version: string;
 	export const NUMBER_OF_PROCESSORS: string;
 	export const OLDPWD: string;
@@ -187,6 +181,7 @@ declare module '$env/dynamic/private' {
 		MSYSTEM_CHOST: string;
 		MSYSTEM_PREFIX: string;
 		NODE: string;
+		NODE_ENV: string;
 		npm_command: string;
 		npm_config_cache: string;
 		npm_config_globalconfig: string;
@@ -203,15 +198,8 @@ declare module '$env/dynamic/private' {
 		npm_lifecycle_event: string;
 		npm_lifecycle_script: string;
 		npm_node_execpath: string;
-		npm_package_dev: string;
-		npm_package_dev_optional: string;
-		npm_package_engines_node: string;
-		npm_package_integrity: string;
 		npm_package_json: string;
 		npm_package_name: string;
-		npm_package_optional: string;
-		npm_package_peer: string;
-		npm_package_resolved: string;
 		npm_package_version: string;
 		NUMBER_OF_PROCESSORS: string;
 		OLDPWD: string;
--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,11 +1,85 @@
 <script>
-  // Write your JS here, or import other files
+  import { onMount } from 'svelte';
+  import * as d3 from 'd3';
+
+  onMount(() => { 
+    async function fetchData() {
+        const response = await fetch('filtered_pitch_data.csv');
+        const pitchData = await response.text();
+        const parsedData = d3.csvParse(pitchData);
+
+        var svg = d3.select("svg"),
+              margin = 200,
+              width = svg.attr("width") - margin,
+              height = svg.attr("height") - margin
+
+        var xScale = d3.scaleLinear().domain([-3, 3]).range([0, width]);
+        var yScale = d3.scaleLinear().domain([-1, 5]).range([height, 0]);
+
+        svg.append('text')
+              .attr('x', width/2 + 100)
+              .attr('y', height - 15 + 150)
+              .attr('text-anchor', 'middle')
+              .style('font-family', 'Helvetica')
+              .style('font-size', 12)
+              .text('Plate Location');
+        svg.append('text')
+              .attr('text-anchor', 'middle')
+              .attr('transform', 'translate(60,' + height + ')rotate(-90)')
+              .style('font-family', 'Helvetica')
+              .style('font-size', 12)
+              .text('Pitch Height');
+
+        const tooltip = d3.select("#my_dataviz")
+          .append("div")
+          .style("opacity", 0)
+          .attr("class", "tooltip")
+          .style("background-color", "white")
+          .style("border", "solid")
+          .style("border-width", "1px")
+          .style("border-radius", "5px")
+          .style("padding", "10px")
+
+        const mouseover = function(event, d) {
+          tooltip
+            .style("opacity", 1)
+        }
+
+        const mousemove = function(event, d) {
+          tooltip
+            .html("Velocity: " + d['RelSpeed'])
+            .style("left", (event.x)/2 + "px") 
+            .style("top", (event.y)/2 + "px")
+        }
+
+        const mouseleave = function(event,d) {
+          tooltip
+            .transition()
+            .duration(200)
+            .style("opacity", 0)
+        }
+
+        svg.append('g')
+          .selectAll("dot")
+          .data(parsedData)
+          .enter()
+          .append("circle")
+            .attr("cx", function (d) { return xScale(d['PlateLocSide']); } )
+            .attr("cy", function (d) { return yScale(d['PlateLocHeight']); } )
+            .attr("r", 2)
+            .attr("transform", "translate(" + 100 + "," + 100 + ")")
+            .style("fill", "#CC0000")
+          .on("mouseover", mouseover)
+          .on("mousemove", mousemove)
+          .on("mouseleave", mouseleave);
+    }
+
+    fetchData();
+  });
 </script>
 
 <main>
-  <h1>Svelte template</h1>
-
-  <p>Write your HTML here</p>
+  <h1>Pitch Location Visualization</h1>
 </main>
 
 <style>
