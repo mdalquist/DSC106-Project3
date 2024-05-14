256950c34712914de501336203bba3096463e259
Seanna-Marie <97414883+Seanna-Marie@users.noreply.github.com>
2024-05-13 19:05:17 -0700

    Update svelte.config.js
    change paths to match directories and folder names

--- svelte.config.js
+++ svelte.config.js
@@ -9,8 +9,8 @@ const config = {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
+			pages: 'components',
+			assets: 'static',
 			fallback: '404.html',
 			precompress: false,
 			strict: true
