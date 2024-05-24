9757ed943f1bbacdd95afa00c639e375342aa59c
Mostafa Elfaki <mostafa@Mostafas-MacBook-Pro-688.local>
2024-05-24 16:17:59 -0700

    Fixing modified and untracked files

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -2,160 +2,36 @@
   import { onMount } from 'svelte';
   import * as d3 from 'd3';
 
-  onMount(() => { 
-    async function fetchData() {
-        const response = await fetch('filtered_pitch_data.csv');
-        const pitchData = await response.text();
-        const parsedData = d3.csvParse(pitchData);
+  var data = [
+      { year: "2006", alex: "104", mindy: "152", sean: "90", karen: "162" },
+      { year: "2007", alex: "122", mindy: "184", sean: "99", karen: "143" },
+      { year: "2008", alex: "50", mindy: "201", sean: "127", karen: "114" },
+      { year: "2009", alex: "150", mindy: "134", sean: "139", karen: "80" }
+  ];
 
-        var svg = d3.select("svg"),
-              margin = 200,
-              width = svg.attr("width") - margin,
-              height = svg.attr("height") - margin
+  var colors = ["#C9D6DF", "#F7EECF", "#E3E1B2", "#F9CAC8"];
 
-        var xScale = d3.scaleLinear().domain([-3, 3]).range([0, width]);
-        var yScale = d3.scaleLinear().domain([-1, 5]).range([height, 0]);
+  var stack = d3.stack().keys(["alex", "mindy", "sean", "karen"]);
+  var dataset = stack(data.map(d => ({
+    x: d3.timeParse("%Y")(d.year),
+    y: [+d.alex, +d.mindy, +d.sean, +d.karen]
+  })));
 
-        svg.append('text')
-              .attr('x', width/2 + 100)
-              .attr('y', height - 15 + 150)
-              .attr('text-anchor', 'middle')
-              .style('font-family', 'Helvetica')
-              .style('font-size', 12)
-              .text('Plate Location');
-        svg.append('text')
-              .attr('text-anchor', 'middle')
-              .attr('transform', 'translate(60,' + height + ')rotate(-90)')
-              .style('font-family', 'Helvetica')
-              .style('font-size', 12)
-              .text('Pitch Height');
-
-        var colorScale = d3.scaleOrdinal()
-          .domain(["Fastball", "Changeup", "Curveball", "Slider"]) 
-          .range(["red", "green", "blue", "orange"]); 
+  
 
 
-        var div = d3.select("body").append("div")   // we also want to display data upon the hovering
-                      .attr("class", "tooltip")
-                      .style("opacity", 0);
 
-        svg.append('g')
-          .selectAll("dot")
-          .data(parsedData)
-          .enter()
-          .append("circle")
-            .attr("cx", function (d) { return xScale(d['PlateLocSide']); } )
-            .attr("cy", function (d) { return yScale(d['PlateLocHeight']); } )
-            .attr("r", 2)
-            .attr("transform", "translate(" + 100 + "," + 100 + ")")
-            .style("fill", function (d) { return colorScale(d['TaggedPitchType'])})
-
-            .on('mouseover', function (event, d) {
-                d3.select(this).transition()
-                  .duration('100')
-                  .attr("r", 7);          //increases the size of the point on hover
-
-
-            // lets make div appear 
-
-                div.transition()
-                    .duration(100)
-                    .style("opacity", 1); 
-
-                div.html('Batter: ' + d['Batter'] + '<br>Team: ' + d['BatterTeam'] + '<br>Pitch Type: ' 
-                        + d['TaggedPitchType'] + '<br>Velocity: ' + d['RelSpeed'] + '<br>Induced Vertical Break: '
-                        + d['InducedVertBreak'] + '<br>Horizontal Break: ' + d['HorzBreak'] + '<br>Pitch Result: '
-                        + d['PitchCall'] + '<br>Play Result: ' + d['PlayResult'])
-                  .style("left", (event.x + 10) + "px")
-                  .style("top", (event.y - 15) + "px");
-                
-            })         
-                
-            .on('mouseout', function (event, d) {
-              
-                d3.select(this).transition()
-                  .duration('200')
-                  .attr("r", 2);                    //Returns the size of the point when not hovering
-            
-            // lets make the div disappear just like the size
-
-                div.transition()
-                  .duration(100)
-                  .style("opacity", 0);
-
-            })
-          
-
-          
-        
-          ;
-
-        svg.append("g")
-         .attr("transform", "translate(" + 100 + "," + 300 + ")")
-         .call(d3.axisBottom(xScale));
-        
-        svg.append("g")
-          .attr("transform", "translate(" + 100 + "," + 100 + ")")
-          .call(d3.axisLeft(yScale))
-          
-          ;
-
-        svg.append('rect')
-          .attr('x', xScale(-8.5/12))
-          .attr('y', yScale(3.5))
-          .attr("transform", "translate(" + 100 + "," + 100 + ")")
-          .attr('width', xScale(17/12) - xScale(0))
-          .attr('height', yScale(0) - yScale(2))
-          .attr('stroke', 'black')
-          .attr('fill', 'none')
-          .style('stroke-width', '2px')
-          
-          
-          
-          
-          
-          ;
-    }
-
-    fetchData();
-  });
 </script>
 
 <main>
-
-  
-    <h1 style="font-family: 'Bebas Neue', sans-serif">Pitch Location Visualization</h1>
-    <hr style="width: 50%; margin: auto; margin-top: 5px; border-top: 2px solid black;">
-    <body></body>
-
-    <style>
-
-        body {
-           
-            text-align: center; /* Center text horizontally */
-            background-color: RGB(255, 255, 255);
-            
-        
-        }
-
-    </style>
-
-
-
-    
-
-    
-
+  <h1>Hello</h1>
+  <body></body>
 </main>
 
 <style>
-
   @import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
   @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
   body {
     font-family: "Bebas Neue", sans-serif;
-  }  
-
-
-
+  }
 </style>
